0b641ccdc71f16ab3cc859b301aae93e16fbe71a
taijulsir <princetaijul123@gmail.com>
2023-11-26 23:29:10 +0600

    added get method based on email for propertycollection

--- index.js
+++ index.js
@@ -142,6 +142,13 @@ async function run() {
       const result = await propertyCollection.find(query).toArray()
       res.send(result)
     })
+    app.get('/agentProperties/:email',async(req,res)=>{
+      const email = req.params.email;
+      const query = {agentEmail: email}
+    
+      const result = await propertyCollection.find(query).toArray()
+      res.send(result)
+    })
     app.get('/properties/:id',async(req,res)=>{
       const id= req.params.id;
       const query = {_id: new ObjectId(id)}
